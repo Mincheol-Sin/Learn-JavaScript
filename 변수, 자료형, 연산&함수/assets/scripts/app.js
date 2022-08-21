1454df66dc0b41238e5478736cd9f6751099db09
Mincheol Sin <cheole720@gmail.com>
2022-08-21 21:36:43 +0900

    Feat: 객체와 배열을 사용하여 연산 결과를 저장하는 함수 구현

--- 변수, 자료형, 연산&함수/assets/scripts/app.js
+++ 변수, 자료형, 연산&함수/assets/scripts/app.js
@@ -1,18 +1,69 @@
 // 변수를 선언하는 키워드 const와 let
 const defaultResult = 0; // const: 상수를 선언할 때 사용하며 값 재할당 불가
 let currentResult = defaultResult; // let: 상수가 아닌 변수를 선언할 때 사용하며 값 재할당 가능
+let logEntries = [];
 
-// 함수 추가하기 - function 함수명(매개변수) { 실행문 }
-
+// input 값을 받아서 숫자형으로 반환하는 함수 선언
 function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
-function add() {
-  const enteredNumber = getUserNumberInput();
-  const calcDescription = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber;
+// 중복 코드를 통합하기 위한 외부 함수를 선언하여 각 함수에 추가
+function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
 }
 
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+// 계산기 버튼(덧셈, 뻴셈, 곱셈, 나눗셈) 기능 구현
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteLog("+", initialResult, enteredNumber);
+  writeToLog("ADD", initialResult, enteredNumber, currentResult);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteLog("-", initialResult, enteredNumber);
+  writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteLog("*", initialResult, enteredNumber);
+  writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteLog("/", initialResult, enteredNumber);
+  writeToLog("DIVIDE", initialResult, enteredNumber, currentResult);
+}
+
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
